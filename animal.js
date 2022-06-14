337aacd4285f3429d3c355e20edde284a5df9aa7
NeehalSaleh <nssalmuways@sm.imamu.edu.sa>
2022-06-14 21:33:28 +0300

    example

---
+++ animal.js
@@ -0,0 +1,19 @@
+
+class animal{
+    name;
+     tybe;
+    constructor(name, tybe){
+        this.name=name;
+        this.tybe=tybe;
+    }
+  
+
+ Name(){
+         console.log(this.name, this.tybe);
+    
+     }
+  
+}
+//var bb=new animal("mm","cat");
+//bb.Name();
+export default animal;
---
+++ app.js
@@ -0,0 +1,5 @@
+
+import animal from './animal.js';
+var bb=new animal("animal","cat");
+bb.Name(); 
+
